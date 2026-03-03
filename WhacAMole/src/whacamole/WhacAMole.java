73bfcdbaf37f7b655ee7a31ecd1bb887f3243677
ficarra_gabriele <ficarra.gabriele@franchettisalviani.net>
2026-03-03 12:48:13 +0100

    V1.2

--- WhacAMole/src/whacamole/WhacAMole.java
+++ WhacAMole/src/whacamole/WhacAMole.java
@@ -14,7 +14,8 @@ public class WhacAMole {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        TalpaVisual t = new TalpaVisual();
+        t.setVisible(true);
     }
     
 }
