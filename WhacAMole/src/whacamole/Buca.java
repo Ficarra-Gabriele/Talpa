cdece61075fdb81bdfd5d312b81ae0446fe1f85a
ficarra_gabriele <ficarra.gabriele@franchettisalviani.net>
2026-03-03 10:28:04 +0100

    VersionePrimaria
    Ho messo le prime tre classi con i metodi e attributi base senza funzioni particolari

---
+++ WhacAMole/src/whacamole/Buca.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package whacamole;
+
+/**
+ *
+ * @author ficarra.gabriele
+ */
+public class Buca {
+    
+    String idBuca;
+    boolean occupata;
+    
+    public void colpitaVuota(){
+        
+    }
+}
