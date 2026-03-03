9b26a2c5ef3acecd44a089f584b98e6196242060
Ficarra-Gabriele <ficarra.gabriele@franchettisalviani.net>
2026-03-03 20:20:21 +0100

    Adattamento3
    ho adattato tutto il gestore

--- WhacAMole/src/whacamole/GestoreGioco.java
+++ WhacAMole/src/whacamole/GestoreGioco.java
@@ -4,9 +4,8 @@
  */
 package whacamole;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
+import java.util.*;
+import javax.swing.Timer;
 
 /**
  *
@@ -14,23 +13,55 @@ import java.util.Timer;
  */
 public class GestoreGioco {
 
-    Timer durataSessione;
-    int livelloDifficoltà;
-    int numeroBuche;
-    Giocatore g;
-    List<Buca> buche = new ArrayList<>();
-    List<Classifica> registro = new ArrayList<>();
+    private List<Buca> buche;
+    private Giocatore giocatore;
+    private Classifica registro;
+    private Random random = new Random();
 
-    public void avviaGioco() {
-
+    public GestoreGioco(int nBuche) {
+        this.buche = new ArrayList<>();
+        for (int i = 0; i < nBuche; i++) {
+            buche.add(new Buca(i));
+        }
+        this.giocatore = new Giocatore();
+        this.registro = new Classifica();
     }
 
-    public void calcolaPunteggio() {
+    public void selezionaBucaLibera() {
+        for (Buca singolaBuca : buche) {
+            singolaBuca.svuota();
+        }
+        int ind = random.nextInt(buche.size());
+        TipoTalpa tipoScelto;
+        double probabilita = random.nextDouble();
+        if (probabilita > 0.8) {
+            tipoScelto = TipoTalpa.BIANCA;
+        } else {
+            tipoScelto = TipoTalpa.NORMALE;
+        }
 
+        Talpa nuovaTalpa = new Talpa(tipoScelto);
     }
 
-    public void selezionaBuca() {
+    public int calcolaPunteggio(int idBuca) {
 
+        Buca b = buche.get(idBuca);
+        if (b.isOccupata()) {
+            int punti = b.getTalpa().getPunteggioValore();
+            giocatore.aggiungiPunti(punti);
+            b.svuota();
+            return punti;
+        } else {
+            giocatore.talpaMancata();
+            return -2;
+        }
     }
-    
+
+    public List<Buca> getBuche() {
+        return buche;
+    }
+
+    public int getPunteggioTotale() {
+        return giocatore.getPunteggio();
+    }
 }
